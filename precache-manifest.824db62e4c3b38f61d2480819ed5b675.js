b78e57168343fdb8bc94fffaabca7e0ec9142231
Sierra Potvin <spotv101@uottawa.ca>
2020-09-11 03:55:33 +0000

    Updates

---
+++ precache-manifest.824db62e4c3b38f61d2480819ed5b675.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d108a932a9a31946dc485f88cb12761",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9eaeaff6ecd2c1e0d16",
+    "url": "/static/css/2.cbdddf7c.chunk.css"
+  },
+  {
+    "revision": "960e0b82f2947a29bd84",
+    "url": "/static/css/main.814f001f.chunk.css"
+  },
+  {
+    "revision": "e9eaeaff6ecd2c1e0d16",
+    "url": "/static/js/2.e2383d7d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.e2383d7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "960e0b82f2947a29bd84",
+    "url": "/static/js/main.74478e00.chunk.js"
+  },
+  {
+    "revision": "be8a2fe983920ea9d527",
+    "url": "/static/js/runtime-main.508be718.js"
+  },
+  {
+    "revision": "d1f6f4f58243ca41cb4bfeb5cf9d056a",
+    "url": "/static/media/BulbBG.d1f6f4f5.png"
+  },
+  {
+    "revision": "4007f2ac5bc072fbbe334a58f785b8f4",
+    "url": "/static/media/BulbBG2.4007f2ac.png"
+  },
+  {
+    "revision": "117e472f6ba43030acd194d53b7b7967",
+    "url": "/static/media/BulbBGGrey.117e472f.png"
+  },
+  {
+    "revision": "a11229b8807d6d282ae49baecb682b67",
+    "url": "/static/media/Rocket.a11229b8.jpg"
+  },
+  {
+    "revision": "e423dc98fd050f64411f9645c8e29530",
+    "url": "/static/media/abstract-gradient.e423dc98.jpg"
+  },
+  {
+    "revision": "51a4ccfc94ef11147817753aaebb986c",
+    "url": "/static/media/planeImage.51a4ccfc.jpg"
+  }
+]);
